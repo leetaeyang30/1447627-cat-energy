adb19f16f83304595a093c042c90627bd1277d7d
Rustam <rustam.sb@yandex.ru>
2022-04-21 21:52:47 +0300

    feat: much

--- source/js/main-navigation.js
+++
@@ -1,9 +0,0 @@
-let navMain = document.querySelector('.main-navigation');
-let navToggle = navMain.querySelector('.main-navigation__toggle');
-
-navMain.classList.remove('main-navigation--nojs');
-
-navToggle.addEventListener('click', function() {
-  navMain.classList.toggle('main-navigation--closed');
-  navMain.classList.toggle('main-navigation--opened');
-});
